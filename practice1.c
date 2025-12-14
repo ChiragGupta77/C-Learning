7dd3cdbd8e9912e30d62fbd6b3219fe9cf540a95
ChiragGupta77 <141412404+ChiragGupta77@users.noreply.github.com>
2025-12-14 15:44:25 +0530

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(){
     int number;
-    printf("What's you favourite funny number\n");
+    printf("What's you favourite number\n");
 
 /* %d --> used for int
    %c --> used for char 
    %f --> used for float */ 
        
     scanf("%d", &number); // scanf is for input
-    printf("%d is your favourite funny number\nGood Bye!!", number);
+    printf("%d is your favourite number\nGood Bye!!", number);
     return 0;
 }
