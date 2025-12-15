6da066e47dad5c247c1ce643bccf630799082762
ChiragGupta77 <141412404+ChiragGupta77@users.noreply.github.com>
2025-12-15 20:00:29 +0530

    Add files via upload

---
+++ Practice_Codes/task5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Kindly enter a number:");
+    scanf("%d", &a);
+    int b = a%7;
+    if (b==0){
+        printf("%d is divisible by 7", a);
+    }
+    else{
+        printf("%d is not divisible by 7", a);
+    }
+    return 0;
+}
